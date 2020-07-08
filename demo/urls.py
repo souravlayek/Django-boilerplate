29eb715744be4ecf374669611e5c5fadffecc5f7
Sourav Layek <souravlayek11@gmail.com>
2020-07-09 01:37:35 +0530

    updtaed urls.py for static url and media url

--- demo/urls.py
+++ demo/urls.py
@@ -1,5 +1,5 @@
 from django.conf import settings
-
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 from .views import home
@@ -14,3 +14,7 @@ if settings.DEBUG:
     urlpatterns = [
         path('__debug__/', include(debug_toolbar.urls)),
     ] + urlpatterns
+    urlpatterns += static(settings.STATIC_URL,
+                        document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
